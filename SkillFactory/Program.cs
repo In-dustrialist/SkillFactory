103f4974db024c651640693e34f243e5ac0a6560
In-dustrialist <pinaevmsk@gmail.com>
2023-05-04 17:37:33 +0300

    Full First Unit

--- SkillFactory/Program.cs
+++ SkillFactory/Program.cs
@@ -10,8 +10,16 @@ namespace SkillFactory
     {
         static void Main(string[] args)
         {
-            string NewVar;
-            NewVar = MyName;
+            const string MyName = "Eugene";
+            
+            Console.WriteLine(MyName);
+            Console.WriteLine("\t Hello? \n World");
+            Console.WriteLine("\t Im 33");
+            Console.WriteLine("\t My name is \n {0}", MyName);
+            Console.WriteLine("\u0040");
+            Console.WriteLine("\x23");
+
+            Console.ReadKey();
         }
     }
 }
