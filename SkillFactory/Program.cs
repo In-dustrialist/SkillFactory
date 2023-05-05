b93a9bf96e759345d2873c57914e514cdcf24c44
In-dustrialist <pinaevmsk@gmail.com>
2023-05-05 12:37:55 +0300

    Task 3.4.3

--- SkillFactory/Program.cs
+++ SkillFactory/Program.cs
@@ -14,13 +14,19 @@ namespace SkillFactory
             byte MyAge = 33;
             bool HaveIApet = false;
             double MyShoeSize = 42.5;
-
+           
             Console.WriteLine("My name is " + MyName);
             Console.WriteLine("MyAge " + MyAge);
             Console.WriteLine("Do I have a pet? " + HaveIApet);
             Console.WriteLine("My shoe size is " + MyShoeSize);
 
             Console.ReadKey();
+        }
+        enum ProgramData
+        {
+            Red = 100, 
+            Yellow = 200, 
+            Green = 300
         }
     }
 }
