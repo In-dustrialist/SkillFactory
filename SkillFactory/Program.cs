05398a3401276a957b8b0530bd178153f797e3ea
In-dustrialist <pinaevmsk@gmail.com>
2023-05-06 23:45:23 +0300

    Last task Unit 3.7.

--- SkillFactory/Program.cs
+++ SkillFactory/Program.cs
@@ -8,43 +8,35 @@ namespace SkillFactory
 {
     internal class Program
     {
+        enum DaysOfWeek : byte
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday
+        }
         static void Main(string[] args)
         {
-            string MyName = "Eugene";
-            byte MyAge = 33;
-            bool HaveIApet = false;
-            double MyShoeSize = 42.5;
-           
-            Console.WriteLine("My name is " + MyName);
-            Console.WriteLine("MyAge " + MyAge);
-            Console.WriteLine("Do I have a pet? " + HaveIApet);
-            Console.WriteLine("My shoe size is " + MyShoeSize);
+            Console.Write("Enter your name: ");
+            string Name = Convert.ToString(Console.ReadLine());
 
-            
-            Console.WriteLine("\t Hello,\n World!"); //Task 3.5.2
-            Console.WriteLine();
+            Console.Write("Enter your age: ");
+            byte Age = Convert.ToByte(Console.ReadLine());
 
-            Console.WriteLine($"Im {MyAge}"); //Task 3.5.3
-            Console.WriteLine();
+            Console.WriteLine("Your name is {0} and age is {1}", Name, Age);
 
-            var name = "Jane"; //Task 3.5.4
-            var age = 27;
-            var favcolor = "black";
-            Console.WriteLine("{0} \n {1} \n {2}", name, age, favcolor);
-            Console.WriteLine();
+            Console.Write("What is your favorite day of week? ");
+            DayOfWeek Day = (DayOfWeek) int.Parse(Console.ReadLine());
+            Console.WriteLine("Your favorite day is {0}", Day);
 
-            string str = Console.ReadLine(); //Task 3.5.5
-            Console.WriteLine();
+            Console.Write("Enter your birthdate: ");
+            var birthdate = (Console.ReadLine());
+            Console.WriteLine("Your birthdate is {0}", birthdate);
 
             Console.ReadKey();
         }
-        enum ProgramData //Task 3.4.3
-        {
-            Red = 100, 
-            Yellow = 200, 
-            Green = 300
-        }
-       
     }
 
-}
+} 
