f6b05f0cea4d8c2a5c197b12ba8941d298d193d6
In-dustrialist <pinaevmsk@gmail.com>
2023-05-20 09:46:50 +0300

    Unit 4.5.6

--- SkillFactory/Program.cs
+++ SkillFactory/Program.cs
@@ -14,42 +14,44 @@ namespace SkillFactory
     {
         static void Main(string[] args)
         {
-            ///Unit 4.5.5
+            ///Unit 4.5.6
 
             {
-                (string FirstName, string LastName, string Login, int LoginLength, bool HasPet, double Age, string[] favcolors) User;
+                for (int anketa = 0; anketa < 3; anketa++)
+                {
 
-                Console.Write("Enter your first name: ");
-                User.FirstName = (Console.ReadLine());
-                Console.WriteLine();
+                    (string FirstName, string LastName, string Login, int LoginLength, bool HasPet, double Age, string[] favcolors) User;
 
-                Console.Write("Enter your last name: ");
-                User.LastName = (Console.ReadLine());
-                Console.WriteLine();
+                    Console.WriteLine("Enter your first name: ");
+                    User.FirstName = (Console.ReadLine());
 
-                Console.Write("Enter your login: ");
-                User.Login = (Console.ReadLine());
-                Console.WriteLine();
-                
-                int LoginLength = User.Login.Length;
-                Console.Write("Your login length: {0}", LoginLength);
-                Console.WriteLine();
-                Console.WriteLine();
 
-                Console.Write("Do you have a Pet? Yes or Not?");
-                var Pet = Console.ReadLine();
-                if (Pet == "Yes"){ User.HasPet = true; Console.WriteLine("Yes"); }
-                if (Pet != "Yes") { User.HasPet = false; Console.WriteLine("No"); }
+                    Console.WriteLine("Enter your last name: ");
+                    User.LastName = (Console.ReadLine());
 
-                Console.Write("Enter your age: ");
-                User.Age = Convert.ToDouble(Console.ReadLine());
-                Console.WriteLine();
 
-                User.favcolors = new string [3];
-                Console.Write("Enter your three favorite colors: ");
-                for (int i = 0; i < User.favcolors.Length; i++) { User.favcolors[i] = Console.ReadLine(); }
-                Console.WriteLine();
+                    Console.WriteLine("Enter your login: ");
+                    User.Login = (Console.ReadLine());
 
+
+                    int LoginLength = User.Login.Length;
+                    Console.WriteLine("Your login length: {0}", LoginLength);
+
+
+                    Console.WriteLine("Do you have a Pet? Yes or Not?");
+                    var Pet = Console.ReadLine();
+                    if (Pet == "Yes") { User.HasPet = true; }
+                    if (Pet != "Yes") { User.HasPet = false; }
+
+                    Console.WriteLine("Enter your age: ");
+                    User.Age = Convert.ToDouble(Console.ReadLine());
+
+
+                    User.favcolors = new string[3];
+                    Console.WriteLine("Enter your three favorite colors: ");
+                    for (int i = 0; i < User.favcolors.Length; i++) { User.favcolors[i] = Console.ReadLine(); }
+
+                }
 
                 Console.ReadKey();
             }
