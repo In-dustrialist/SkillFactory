c1ea506de847c92597e4d7515bbb6e13a5ec2663
Eugene P <pinaevmsk@gmail.com>
2023-05-22 00:44:22 +0400

    Unit 5.1.4

--- SkillFactory/Program.cs
+++ SkillFactory/Program.cs
@@ -14,47 +14,41 @@ namespace SkillFactory
     {
         static void Main(string[] args)
         {
-            ///Unit 4.5.6
+            ///Unit 5.1.4
 
             {
-                for (int anketa = 0; anketa < 3; anketa++)
-                {
-
-                    (string FirstName, string LastName, string Login, int LoginLength, bool HasPet, double Age, string[] favcolors) User;
-
-                    Console.WriteLine("Enter your first name: ");
-                    User.FirstName = (Console.ReadLine());
-
-
-                    Console.WriteLine("Enter your last name: ");
-                    User.LastName = (Console.ReadLine());
-
-
-                    Console.WriteLine("Enter your login: ");
-                    User.Login = (Console.ReadLine());
-
-
-                    int LoginLength = User.Login.Length;
-                    Console.WriteLine("Your login length: {0}", LoginLength);
-
-
-                    Console.WriteLine("Do you have a Pet? Yes or Not?");
-                    var Pet = Console.ReadLine();
-                    if (Pet == "Yes") { User.HasPet = true; }
-                    if (Pet != "Yes") { User.HasPet = false; }
-
-                    Console.WriteLine("Enter your age: ");
-                    User.Age = Convert.ToDouble(Console.ReadLine());
-
-
-                    User.favcolors = new string[3];
-                    Console.WriteLine("Enter your three favorite colors: ");
-                    for (int i = 0; i < User.favcolors.Length; i++) { User.favcolors[i] = Console.ReadLine(); }
-
-                }
-
+                Console.WriteLine("Test new Function");
+              
                 Console.ReadKey();
             }
+            
+        }
+        static void ShowColor()
+        {
+            Console.WriteLine("Enter your favotite color: ");
+
+            var color = Console.ReadLine();
+
+            switch (Console.ReadLine())
+            {
+                case "red":
+
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is red!");
+                    break;
+
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is green!");
+                    break;
+            }
+
+            Console.ReadKey();
+
         }
     }
 }
