340b2194854d8f632a4d21fc8342ec2c5c1d1b6d
Eugene P <pinaevmsk@gmail.com>
2023-06-04 09:12:47 +0400

    SKILL FACTORY

--- SkillFactory/Program.cs
+++ SkillFactory/Program.cs
@@ -23,77 +23,12 @@ namespace SkillFactory
     {
         public static void Main(string[] args)
         {
-            FillTheForm();
+            string b = "d";
+            int a = int.Parse(b);
+           
+            Console.WriteLine(a + b);
             Console.ReadKey();
         }
-        static (string Name, string LastName, int Age) FillTheForm()
-        {
-            (string Name, string LastName, int Age) User;
 
-            Console.WriteLine("What is your name?");
-            User.Name = (Console.ReadLine());
-
-            Console.WriteLine("What is your last name?");
-            User.LastName = Console.ReadLine();
-
-            Console.WriteLine("How old are you?");
-
-            do
-            {
-                User.Age = Convert.ToInt32(Console.ReadLine());
-            }
-            while (CheckValue(User.Age));
-
-            Console.WriteLine("Do you have a Pet? Yes or No");
-            var result = Console.ReadLine();
-
-            if (result == "Yes")
-            {
-                Console.WriteLine("How many pets do you have?");
-                int Pets;
-                do
-                {
-                    Pets = Convert.ToInt32(Console.ReadLine());
-                }
-                while (CheckValue(Pets));
-                GetInfo(Pets);
-            }
-
-            Console.WriteLine("How many favorite colors do you have? ");
-            int Colors;
-            do
-            {
-                Colors = Convert.ToInt32(Console.ReadLine());
-            }
-            while (CheckValue(Colors));
-            GetInfo(Colors);
-
-            Console.WriteLine($"\n Your Name is: {User.Name} \t  Your Last Name is: {User.LastName} \t You are {User.Age} years old");
-
-            return User;
-        }
-
-        static string[] GetInfo(int num)
-        {
-            var result = new string[num];
-            int n = 1;
-            for (int i = 0; i < result.Length; i++)
-            {
-                Console.Write("{0}: ", n);
-                n++;
-                result[i] = Console.ReadLine();
-            }
-            return result;
-        }
-
-        static bool CheckValue(int num)
-        {
-            if (num <= 0)
-            {
-                Console.WriteLine("The value is incorrect, please try again!");
-                return true;
-            }
-            else return false;
-        }
     }
 }
