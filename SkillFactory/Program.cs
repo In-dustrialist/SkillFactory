5b846a5c34cd6984b85c70e77f98896e777a7a70
Eugene P <pinaevmsk@gmail.com>
2023-07-27 00:32:44 +0400

    Unit 6.5.2

--- SkillFactory/Program.cs
+++ SkillFactory/Program.cs
@@ -10,27 +10,44 @@ namespace SkillFactory
 {
     internal class Program
     {
+        class Triangular
+        {
+            public int side1;
+            public int side2;
+            public int side3;
+
+            static int perimeter(int side1, int side2, int side3)
+            {
+                int result = side1 + side2 + side3;
+                return result;
+            }
+        }
+        class Circle
+        {
+            public double radius;
+            static double square(double radius)
+            {
+                double p = 3.14;
+                double s = radius * radius;
+                double result = s * p;
+                return result;
+            }
+            
+        }
+        class Square
+        {
+            public int side;
+
+            static int perimeter(int side)
+            {
+                int result = side * 4;
+                return result;
+            }
+        }
 
         public static void Main(string[] args)
         {
-            Employee employee = new Employee();
-            string name = employee?.Department?.Company?.Name ?? "Bank";
 
-
-        }
-        class Employee
-        {
-            public Department Department;
-        }
-
-        class Department
-        {
-            public Company Company;
-        }
-
-        class Company
-        {
-            public string Name;
         }
 
     }
