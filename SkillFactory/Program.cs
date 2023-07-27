55011022ed1b93ec4d549d2dca5801705991f99d
Eugene P <pinaevmsk@gmail.com>
2023-07-27 05:07:26 +0400

    Unit 6.6.1

--- SkillFactory/Program.cs
+++ SkillFactory/Program.cs
@@ -10,44 +10,22 @@ namespace SkillFactory
 {
     internal class Program
     {
-        class Triangular
-        {
-            public int side1;
-            public int side2;
-            public int side3;
-
-            static int perimeter(int side1, int side2, int side3)
-            {
-                int result = side1 + side2 + side3;
-                return result;
-            }
-        }
-        class Circle
-        {
-            public double radius;
-            static double square(double radius)
-            {
-                double p = 3.14;
-                double s = radius * radius;
-                double result = s * p;
-                return result;
-            }
-            
-        }
-        class Square
-        {
-            public int side;
-
-            static int perimeter(int side)
-            {
-                int result = side * 4;
-                return result;
-            }
-        }
-
         public static void Main(string[] args)
         {
 
+        }
+
+        class TrafficLight
+        {
+            private void ChangeColor(string color)
+            {
+
+            }
+
+            public string GetColor()
+            {
+
+            }
         }
 
     }
