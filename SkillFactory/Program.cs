2bf1b53edd687d24dea8060c0a2846f016b17825
Eugene P <pinaevmsk@gmail.com>
2023-07-29 01:31:32 +0400

    Unit 7.1.6

--- SkillFactory/Program.cs
+++ SkillFactory/Program.cs
@@ -14,21 +14,21 @@ namespace SkillFactory
         {
 
         }
-        class Food { }
+        class Obj
+        {
+            private string name;
+            private string owner;
+            private int length;
+            private int count;
 
-        class Fruit : Food { }
-
-        class Vegetable : Food { }
-
-        class Apple : Fruit { }
-
-        class Banana : Fruit { }
-
-        class Pear : Fruit { }
-
-        class Potato : Vegetable { }
-
-        class Carrot : Vegetable { }
+            public Obj(string name, string ownerName, int objLength, int count)
+            {
+                this.name = name;
+                owner = ownerName;
+                length = objLength;
+                this.count = count;
+            }
+        }
     }
 }
 
