3b1b3a3d08288cc987fe60275ba119358937dd5c
Eugene P <pinaevmsk@gmail.com>
2023-07-28 20:11:12 +0400

    Unit 7.1.5

--- SkillFactory/Program.cs
+++ SkillFactory/Program.cs
@@ -14,17 +14,21 @@ namespace SkillFactory
         {
 
         }
-        class Employee
-        {
-            public string Name;
-            public int Age;
-            public int Salary;
-        }
-        class ProjectManager : Employee
-        {public string ProjectName; }
+        class Food { }
 
-        class Developer : Employee
-        { public string ProgrammingLanguage; }
+        class Fruit : Food { }
+
+        class Vegetable : Food { }
+
+        class Apple : Fruit { }
+
+        class Banana : Fruit { }
+
+        class Pear : Fruit { }
+
+        class Potato : Vegetable { }
+
+        class Carrot : Vegetable { }
     }
-    }
+}
 
