7598c0331ba05188feb8e92413b2cbac3536dfcf
Eugene P <pinaevmsk@gmail.com>
2023-07-28 19:53:51 +0400

    Unit 7.1.4

--- SkillFactory/Program.cs
+++ SkillFactory/Program.cs
@@ -14,51 +14,17 @@ namespace SkillFactory
         {
 
         }
-        class User
+        class Employee
         {
-            private string login;
-
-            public string Login
-            {
-                get
-                {
-                    return login;
-                }
-
-                set
-                {
-                    if (value.Length < 3)
-                    {
-                        Console.WriteLine("Login must be at least 3 characters long");
-                    }
-                    else
-                    {
-                        login = value;
-                    }
-                }
-            }
-
-            private string email;
-
-            public string Email
-            {
-                get
-                {
-                    return email;
-                }
-
-                set
-                {
-                    if (!value.Contains('@'))
-                    {
-                        Console.WriteLine("Incorrect adress");
-                    }
-                    else
-                    {
-                        email = value;
-                    }
-                }
-            }
+            public string Name;
+            public int Age;
+            public int Salary;
         }
+        class ProjectManager : Employee
+        {public string ProjectName; }
+
+        class Developer : Employee
+        { public string ProgrammingLanguage; }
     }
-}
+    }
+
