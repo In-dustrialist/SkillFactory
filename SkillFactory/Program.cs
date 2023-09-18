6781acf7e5c318c17fe6f3ec2c3d4ee0ef4f5185
Eugene P <pinaevmsk@gmail.com>
2023-09-18 05:27:46 +0400

    Task 7.2.12

--- SkillFactory/Program.cs
+++ SkillFactory/Program.cs
@@ -3,6 +3,7 @@ using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.ExceptionServices;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,36 +13,28 @@ namespace SkillFactory
 {
     internal class Program
     {
-        class A
+        class Obj
         {
-        public virtual void Display() => Console.WriteLine("A");
-        }
-        class B : A
-        {
-            public new void Display() => Console.WriteLine("B");
-        }
-        class C : A
-        {
-            public override void Display() => Console.WriteLine("C");
-        }
-        class D : B
-        {
-            public new void Display() => Console.WriteLine("D");
-        }
-        class E : C
-        {
-            public new void Display() => Console.WriteLine("E");
-        }
+            public int Value;
 
+            public static Obj operator +(Obj a, Obj b)
+            {
+                return new Obj
+                {
+                    Value = a.Value + b.Value
+                };
+            }
+            public static Obj operator -(Obj a, Obj b)
+            {
+                return new Obj
+                {
+                    Value = a.Value - b.Value
+                };
+            }
+        }
         static void Main(string[] args)
         {
-            A a = new A(); a.Display(); 
-            B b = new B(); b.Display(); 
-            C c = new C(); c.Display(); 
-            D d = new D(); d.Display(); 
-            E e = new E(); e.Display();
-
-            Console.ReadKey();
+            
         }
     }
 }
