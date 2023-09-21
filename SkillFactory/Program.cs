ae6f5a7160c210f0c23133a80754d85a770b81e2
Eugene P <pinaevmsk@gmail.com>
2023-09-22 01:54:53 +0400

    Task 7.5.5

--- SkillFactory/Program.cs
+++ SkillFactory/Program.cs
@@ -8,25 +8,25 @@ namespace ConsoleApp1
 {
     internal class Program
     {
-        class Helper
+        class Obj
         {
-            static void Swap(ref int x, ref int y)
+            public string Name;
+            public string Description;
+
+            public static string Parent;
+            public static int DaysInWeek;
+            public static int MaxValue;
+
+            static Obj()
             {
-                int t = x;
-                x = y;
-                y = t;
-
+                Parent = "System.Object";
+                DaysInWeek = 7;
+                MaxValue = 2000;
             }
+        }
+        static void Main(string[] args)
+        {
 
-            static void Main(string[] args)
-            {
-                int num1 = 3;
-                int num2 = 58;
-
-                Helper.Swap(ref num1, ref num2);
-                Console.WriteLine(num1);
-                Console.WriteLine(num2);
-            }
         }
     }
 }
