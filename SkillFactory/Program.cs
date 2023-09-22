743b3c8a951bdf333a14b34dd88911db6d34e4c3
Eugene P <pinaevmsk@gmail.com>
2023-09-22 04:40:33 +0400

    Task 7.5.9

--- SkillFactory/Program.cs
+++ SkillFactory/Program.cs
@@ -8,25 +8,45 @@ namespace ConsoleApp1
 {
     internal class Program
     {
-        class Obj
-        {
-            public string Name;
-            public string Description;
 
-            public static string Parent;
-            public static int DaysInWeek;
-            public static int MaxValue;
-
-            static Obj()
-            {
-                Parent = "System.Object";
-                DaysInWeek = 7;
-                MaxValue = 2000;
-            }
-        }
         static void Main(string[] args)
         {
+            int num1 = 7;
+            int num2 = -13;
+            int num3 = 0;
 
+            Console.WriteLine(num1.GetNegative()); //-7
+            Console.WriteLine(num1.GetPositive()); //7
+            Console.WriteLine(num2.GetNegative()); //-13
+            Console.WriteLine(num2.GetPositive()); //13
+            Console.WriteLine(num3.GetNegative()); //0
+            Console.WriteLine(num3.GetPositive()); //0
+
+        }
+    }
+    static class IntExtensions
+    {
+        public static int GetNegative(this int sign)
+        {
+            if (sign > 0)
+            {
+                return sign *= -1;
+            }
+            else
+            {
+                return sign;
+            }
+        }
+        public static int GetPositive(this int sign)
+        {
+            if (sign < 0)
+            {
+                return sign *=-1;
+            }
+            else
+            {
+                return sign;
+            }
         }
     }
 }
