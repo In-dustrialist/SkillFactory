e017ba8f8e58edb8e9bab6f8b27b6c961e3ed973
Eugene P <pinaevmsk@gmail.com>
2023-09-22 10:18:39 +0400

    Task 7.6.2

--- SkillFactory/Program.cs
+++ SkillFactory/Program.cs
@@ -8,45 +8,16 @@ namespace ConsoleApp1
 {
     internal class Program
     {
-
+        class Car<T>
+        {
+            public T Engine;
+        }
+        class ElectricEngine { }
+        class GasEngine { }
         static void Main(string[] args)
         {
-            int num1 = 7;
-            int num2 = -13;
-            int num3 = 0;
 
-            Console.WriteLine(num1.GetNegative()); //-7
-            Console.WriteLine(num1.GetPositive()); //7
-            Console.WriteLine(num2.GetNegative()); //-13
-            Console.WriteLine(num2.GetPositive()); //13
-            Console.WriteLine(num3.GetNegative()); //0
-            Console.WriteLine(num3.GetPositive()); //0
 
-        }
-    }
-    static class IntExtensions
-    {
-        public static int GetNegative(this int sign)
-        {
-            if (sign > 0)
-            {
-                return sign *= -1;
-            }
-            else
-            {
-                return sign;
-            }
-        }
-        public static int GetPositive(this int sign)
-        {
-            if (sign < 0)
-            {
-                return sign *=-1;
-            }
-            else
-            {
-                return sign;
-            }
         }
     }
 }
