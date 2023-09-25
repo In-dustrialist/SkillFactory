e44352a15af3cb989935059c73e13441a109c111
Eugene P <pinaevmsk@gmail.com>
2023-09-25 07:55:12 +0400

    Task 8.2.4

--- SkillFactory/Program.cs
+++ SkillFactory/Program.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
+
 
 namespace ConsoleApp1
 {
@@ -15,37 +17,24 @@ namespace ConsoleApp1
         {
 
             GetCatalogs(); //   Вызов метода получения директорий
+            MoveFolder();
         }
 
         static void GetCatalogs()
         {
-            DirectoryInfo dirInfo = new DirectoryInfo("C:\\NFolder");
+            DirectoryInfo dirInfo = new DirectoryInfo(@"C:\Users\pinae\OneDrive\Рабочий стол");
             if (!dirInfo.Exists)
                 dirInfo.Create();
 
-            dirInfo.CreateSubdirectory("NewFolder");
+            dirInfo.CreateSubdirectory("testFolder");
+        }
+        static void MoveFolder()
+        {
 
-            string dirName = "C:\\"; // Прописываем путь к корневой директории Windows "C:\\")
-            if (Directory.Exists(dirName)) // Проверим, что директория существует
-            {
-                Console.WriteLine("Folders:");
-                string[] dirs = Directory.GetDirectories(dirName);  // Получим все директории корневого каталога
-                int amountFolders = dirs.Length;
-                Console.WriteLine(amountFolders);
 
-                Console.WriteLine();
-                Console.WriteLine("Files:");
-                string[] files = Directory.GetFiles(dirName);// Получим все файлы корневого каталога
-                int amountFiles = files.Length;
-
-                Console.WriteLine(amountFiles);
-                Console.WriteLine();
-
-                DirectoryInfo dirInfoDelte = new DirectoryInfo("C:\\NFolder");
-                dirInfo.Delete(true); // Удаление со всем содержимым
-                Console.WriteLine("Deleted");
-            }
-            
+            DirectoryInfo dirInfo = new DirectoryInfo(@"C:\Users\pinae\OneDrive\Рабочий стол\testFolder");
+            dirInfo.Delete(true); // Удаление со всем содержимым
+            Console.WriteLine("Dleted");
+        }
         }
     }
-}
